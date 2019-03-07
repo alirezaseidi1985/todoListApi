fced078df56e5d47cd628be0a11317137319c59c
REZA <alirezaseidi@hotmail.com>
2019-03-07 21:34:53 +0200

    created to do list api

---
+++ src/main/java/org/fasttrackit/damain/ToDoItem.java
@@ -0,0 +1,42 @@
+package org.fasttrackit.damain;
+
+import java.sql.Date;
+
+public class ToDoItem {
+    private long id;
+    private String description;
+    private Date deadline;
+    private boolean done;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(Date deadline) {
+        this.deadline = deadline;
+    }
+}
